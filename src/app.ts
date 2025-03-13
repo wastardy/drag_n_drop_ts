5586521c3057de66ad943602f4f35799ec5a0737
Andrew Kononenko <wastardy.k@gmail.com>
2025-03-13 02:03:34 +0200

    add ProjectList class and invoke 2 instances of this class

--- src/app.ts
+++ src/app.ts
@@ -69,6 +69,47 @@ function autobind(
 }
 //#endregion
 
+//#region project list calss
+class ProjectList {
+  templateElement: HTMLTemplateElement;
+  hostElement: HTMLDivElement;
+  element: HTMLElement;
+
+  constructor(private type: "active" | "finished") {
+    this.templateElement = document.getElementById(
+      "project-list"
+    )! as HTMLTemplateElement;
+
+    this.hostElement = document.getElementById("app")! as HTMLDivElement;
+
+    const importedNode = document.importNode(
+      this.templateElement.content,
+      true
+    );
+
+    this.element = importedNode.firstElementChild as HTMLElement;
+    this.element.id = `${type}-projects`;
+
+    this.attach();
+    this.renderContent();
+  }
+
+  private renderContent() {
+    const listId = `${this.type}-projects-list`;
+
+    this.element.querySelector("ul")!.id = listId;
+
+    this.element.querySelector(
+      "h2"
+    )!.textContent = `${this.type.toUpperCase()} PROJECTS`;
+  }
+
+  private attach() {
+    this.hostElement.insertAdjacentElement("beforeend", this.element);
+  }
+}
+//#endregion
+
 //#region  project input class
 class ProjectInput {
   templateElement: HTMLTemplateElement;
@@ -180,3 +221,5 @@ class ProjectInput {
 
 // instantiate the class
 const projectInput = new ProjectInput();
+const activeProjectList = new ProjectList("active");
+const finishedProjectList = new ProjectList("finished");
