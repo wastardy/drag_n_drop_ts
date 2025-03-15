51aa99f110bc673fbf83c2fc479a1d20fd4590a1
Andrew Kononenko <wastardy.k@gmail.com>
2025-03-15 13:25:55 +0200

    add Draggable and DragTarget interfaces

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,16 @@
+//#region drag and drop interfaces
+interface Draggable {
+  dragStartHandler(event: DragEvent): void;
+  dragEndHandler(event: DragEvent): void;
+}
+
+interface DragTarget {
+  dragOverHandler(event: DragEvent): void;
+  dragHandler(event: DragEvent): void;
+  dragLeaveHandler(event: DragEvent): void;
+}
+//#endregion
+
 //#region project type
 enum ProjectStatus {
   Active,
